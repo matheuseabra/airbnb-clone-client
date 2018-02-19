2e09fc0c206c34a1c1aad11897d4fe48015c7eb4
matheuseabra <matheuseabra@gmail.com>
2018-02-19 12:27:22 -0300

    new components, and added state

--- src/App.js
+++ src/App.js
@@ -1,55 +1,159 @@
 import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 
-let SidebarP = "This function is a valid React component because it accepts a single “props” (which stands for properties) object argument with data and returns a React element. We call such components “functional” because they are literally JavaScript functions."
+
 const AppTitle = "Welcome to MatheusApp"
-const AppDescription = "This is how a my React app works"
+const AppDescription = "Creating UI with components is awesome"
+
+const SidebarTitle = "This is my sidebar title"
+const SidebarDescription = "This function is a valid React component because it accepts a single “props” (which stands for properties) object argument with data and returns a React element. We call such components “functional” because they are literally JavaScript functions."
+
+const FooterTitle = "This is my footer description"
+const FooterDescription = SidebarDescription
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+    console.log('mounted');
+  }
+
+  state = {
+    toggle: true
+  }
+
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{AppTitle}</h1>
-          <p className="App-description"> {AppDescription}</p>
-          <br/>  
-        </header>
-        <MainContent />
-        <Sidebar title="This is a sidebar" body={SidebarP}/>
+        <Header title={AppTitle} description={AppDescription} />
+
+        <div className="container">
+        
+        <Main title="This is my main content title" body="and this is my main content body of text"/>
+        
+         
+        <Sidebar title={SidebarTitle} body={SidebarDescription}/> 
+            
+        </div>
+        <Footer title={FooterTitle} body={FooterDescription}/>
       </div>
     );
   }
 }
 
-class MainContent extends Component {
-  render(){
+class Header extends Component {
+
+  render() {
+
+    const {title, description } = this.props;
+
     return (
-      <div className="MainContent"> 
+      <header className="Header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="Header-title">{this.props.title}</h1>
+          <p className="Header-description">{ this.props.description }</p>
+          <Clock date={new Date()} />  
+      </header>
+
+    );
+  }
+}
+
+class Main extends Component {
+
+
+  render() {
+    return (
+      <main className="MainContent"> 
           <h1 className="MainContent-title">{this.props.title}</h1>
           <p className="MainContent-body">
-          {this.props.body}
+          {SidebarDescription}
          </p>
-
-      </div>
+         
+      </main>
     );
   }
 }
 
 class Sidebar extends Component {
-  render(){
+
+
+  render() {
+
+    const { title, body } =  this.props;
+
     return (
-      <div className="Sidebar"> 
+
+      <aside className="Sidebar"> 
           <h3 className="Sidebar-title">{this.props.title}</h3>
           <p className="Sidebar-body">
-          {this.props.body}.
+          {this.props.body}
          </p>
+         <Button text="Show/Hide sidebar" onClick={this.toggle} />
+      </aside>
+      
+    );
+  }
+}
 
+class Footer extends Component {
+  render() {
+
+    const { title, body } =  this.props;
+
+    return (
+      <footer className="Footer"> 
+          <h3 className="footer-title">{this.props.title}</h3>
+          <p>{this.props.body}</p>
+          <ul className="footer-links">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+         </ul>
+      </footer>
+    );
+  }
+}
+
+class Button extends Component {
+
+  render() {
+    const { text } =  this.props;
+    return (
+      <button type="button" onClick={this.toggle} className="Button">{text}</button>
+    );
+  }
+}
+
+class Clock extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
 }
 
 export default App;
+ 
